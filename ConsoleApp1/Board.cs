45acafd94c9a574175321428463de3745c4f763a
Tony Collins <tjc@eltham-college.org.uk>
2019-12-12 12:50:34 +0000

    Add project files.

---
+++ ConsoleApp1/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Board
+    {
+        private string[] b = new string[9];
+
+        public Board()
+        {
+            for(int x = 0; x < 9; x++)
+            {
+                b[x] = " ";
+            }
+         
+        }
+
+        public Board(string c)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                b[x] = c;
+            }
+
+        }
+
+
+        public void printBoard()
+        {
+
+            Console.Clear();
+            Console.WriteLine(b[0] + "|" + b[1] + "|" + b[2]);
+            Console.WriteLine(b[3] + "|" + b[4] + "|" + b[5]);
+            Console.WriteLine(b[6] + "|" + b[7] + "|" + b[8]);
+
+
+        }
+
+
+        public Boolean addX(int x)
+        {
+            b[x] = "X";
+            return true;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Board uzo;
+
+            uzo = new Board("*");
+
+            uzo.addX(3);
+            uzo.printBoard();
+
+            uzo.addX(5);
+            uzo.printBoard();
+        }
+    }
+}
